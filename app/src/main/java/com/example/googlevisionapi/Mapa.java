c2117a8b454599ab734422313bfc09a648f9ccd4
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2021-03-05 00:40:01 -0500

    ..

--- app/src/main/java/com/example/googlevisionapi/Mapa.java
+++ app/src/main/java/com/example/googlevisionapi/Mapa.java
@@ -1,14 +1,73 @@
 package com.example.googlevisionapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import WebServices.Asynchtask;
+import WebServices.WebService;
 import android.os.Bundle;
 
-public class Mapa extends AppCompatActivity {
+public class Mapa extends AppCompatActivity implements Asynchtask, AdapterView.OnItemClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("http://www.geognos.com/api/en/countries/info/all.json", datos, this, this);
+        ws.execute("");
+
+        lstOpciones.setOnItemClickListener(this);
+        getPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        getPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
     }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Intent intent = new Intent (view.getContext(), activityMapa.class);
+
+        intent.putExtra("listaCooordenadas",listaPaises.get(position).getCoordenadasPais());
+        intent.putExtra("url_imagen",listaPaises.get(position).getUrl_Pais());
+        intent.putExtra("nombre_pais",listaPaises.get(position).getNombrePais());
+
+        startActivityForResult(intent, 0);
+
+    }
+    public void getPermission(String permission){
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!(checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED))
+                ActivityCompat.requestPermissions(this, new String[]{permission}, 1);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[]
+            permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1)
+        {
+            Toast.makeText(this.getApplicationContext(),"OK", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
