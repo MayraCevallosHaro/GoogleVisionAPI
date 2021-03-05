f368e99beb80c118733723c480a51b5ea73d52ab
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2021-03-05 01:03:19 -0500

    json

--- app/src/main/java/com/example/googlevisionapi/PaisesCor.java
+++ app/src/main/java/com/example/googlevisionapi/PaisesCor.java
@@ -1,4 +1,22 @@
 package com.example.googlevisionapi;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PaisesCor {
+    private String nombrePais;
+    private String url_Pais;
+    private double []  coordenadasPais;
+
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
 }
